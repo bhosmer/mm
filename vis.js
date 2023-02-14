246ab348b5f9925e0ff278a0ae26fdce8d61d0c9
Basil Hosmer <basil.hosmer@gmail.com>
2023-02-14 10:41:41 -0500

    no spotlight on animation mats

--- vis.js
+++ vis.js
@@ -742,7 +742,8 @@ export class MatMul {
     this.left.updateLabels(spotlight)
     this.right.updateLabels(spotlight)
     this.result.updateLabels(spotlight)
-    this.anim_mats.map(m => m.updateLabels(spotlight))
+    // this is a bit cluttered
+    // this.anim_mats.map(m => m.updateLabels(spotlight))
   }
 
   setPosition() {
